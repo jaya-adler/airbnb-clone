a2262d288f93828b9ef87c8427de03e6fdf1c149
jaya-adler <jayashankar9741@gmail.com>
2021-11-04 00:50:44 +0530

    Updates

---
+++ precache-manifest.f6867cc5cdc9adbf334f0023977f23c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed051b1f8177de7620449749f697deb7",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "8132f977f5b1aef99b90",
+    "url": "/airbnb-clone/static/css/main.bfc722b0.chunk.css"
+  },
+  {
+    "revision": "00fa4416e65c82fb3157",
+    "url": "/airbnb-clone/static/js/2.424adc15.chunk.js"
+  },
+  {
+    "revision": "eb314788fe3fb32ce23a1d6fa9676554",
+    "url": "/airbnb-clone/static/js/2.424adc15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc3ed740b1cc7a1879fd",
+    "url": "/airbnb-clone/static/js/3.f004fb7d.chunk.js"
+  },
+  {
+    "revision": "8132f977f5b1aef99b90",
+    "url": "/airbnb-clone/static/js/main.47582cce.chunk.js"
+  },
+  {
+    "revision": "9ef59933be7afa580457",
+    "url": "/airbnb-clone/static/js/runtime-main.79e3b7dd.js"
+  },
+  {
+    "revision": "cfa9fd4fb698ac1f35a10f148e397689",
+    "url": "/airbnb-clone/static/media/airbnb.cfa9fd4f.svg"
+  }
+]);
